10a7296c2bd15e949653db78880cda205e246626
project-ncp1 <yr826zyyh8@privaterelay.appleid.com>
2026-01-15 15:39:54 +0900

    function.sh

---
+++ function.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RESULT="/tmp/result.log"
+TMP1=$(basename $0).log
+
+NC='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+BOLD='\033[1m'
+
+BAR(){
+	printf "${BLUE}=%.0s${NC}" {1..80}
+	echo
+}
+
+CODE() {
+	echo -e "${BOLD}${BLUE}[$(basename $0)] $1${NC}"
+}
+
+OK() {
+	echo -e "${GREEN}[OK] $1${NC}" | tee -a "$REPORT"
+}
+
+WARN() {
+	echo -e "${YELLO}[WARN] $1${NC}" | tee -a "$REPORT"
+}
